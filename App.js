bf34170d374e4224263562141cd3b5f7f50e7988
kronosGR <geo.elgeo@gmail.com>
2022-01-30 12:48:05 +0100

    added first style

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={{ padding: 30 }}>
       <View>
-
+        <TextInput
+          placeholder='Course Goal'
+          style={{ borderBottomColor: 'black', borderBottomWidth: 1 }}
+        />
+        <Button title='ADD' />
       </View>
-      <View>
-
-      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
