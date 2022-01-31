a65f0d0632fc792283bba2854efb70873b264809
kronosGR <geo.elgeo@gmail.com>
2022-01-31 12:44:31 +0100

    added state and events

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoad] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoad(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals], enteredGoal)
+    setEnteredGoad('');
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.input} />
-        <Button title='ADD' />
+        <TextInput
+          placeholder='Course Goal'
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title='ADD' onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+
+      </View>
     </View>
   );
 }
