97d366ae4ee5a9a75ebf2184e7e6f5816e6bd86a
KronosGR <geo.elgeo@gmail.com>
2022-02-01 12:25:46 +0100

    remove item

--- App.js
+++ App.js
@@ -7,21 +7,27 @@ import GoalItem from './components/GoalItem';
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
-
   const addGoalHandler = (goalTitle) => {
     setCourseGoals((currentGoals) => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle },
     ]);
   };
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} onDelete={()=> console.log('lllll')} />
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+        renderItem={(itemData) => (
+          <GoalItem id={itemData.item.id} title={itemData.item.value} onDelete={removeGoalHandler} />
+        )}
       />
     </View>
   );
@@ -29,5 +35,4 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: { padding: 30 },
-  
 });
