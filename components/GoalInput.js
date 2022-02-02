8b4ef0452623f035d47abc9aff1927c36b32cc0e
KronosGR <geo.elgeo@gmail.com>
2022-02-02 13:22:19 +0100

    closing the modal and cancel

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -8,6 +8,11 @@ function GoalInput(props) {
     setEnteredGoad(enteredText);
   };
 
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoal);
+    setEnteredGoad('');
+  };
+
   return (
     <Modal visible={props.visible} animationType='slide'>
       <View style={styles.inputContainer}>
@@ -17,7 +22,8 @@ function GoalInput(props) {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title='ADD' onPress={props.onAddGoal.bind(this, enteredGoal)} />
+        <Button title='CANCEL' color='red' onPress={props.onCancel} />
+        <Button title='ADD' onPress={addGoalHandler} />
       </View>
     </Modal>
   );
@@ -27,9 +33,15 @@ const styles = StyleSheet.create({
   inputContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex:1
+    flex: 1,
   },
-  input: { borderColor: 'black', borderWidth: 1, padding: 10, width: '80%', marginBottom:10 },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+    marginBottom: 10,
+  },
 });
 
 export default GoalInput;
